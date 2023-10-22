6ec2fc498af2447ac09f424f6663e96abda29b34
Nicolay Roness <nicroness@gmail.com>
2023-10-22 19:44:48 +0200

    add resolving and binding

--- jlox/Jlox.java
+++ jlox/Jlox.java
@@ -69,6 +69,9 @@ public class Jlox {
             return;
         }
 
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
         interpreter.interpret(statements);
     }
 
