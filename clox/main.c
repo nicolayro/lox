46f7bc4f792958d815d98da12513268a0ad09571
Nicolay Roness <nicroness@gmail.com>
2023-10-28 23:07:48 +0200

    add virtual machine for interpreting byte code

--- clox/main.c
+++ clox/main.c
@@ -1,8 +1,11 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char* argv[]) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
@@ -10,10 +13,25 @@ int main(int argc, char* argv[]) {
     writeChunk(&chunk, OP_CONST, 124);
     writeChunk(&chunk, constant, 124);
 
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONST, 124);
+    writeChunk(&chunk, constant, 124);
+
+    writeChunk(&chunk, OP_ADD, 124);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONST, 124);
+    writeChunk(&chunk, constant, 124);
+
+    writeChunk(&chunk, OP_DIVIDE, 124);
+    writeChunk(&chunk, OP_NEGATE, 124);
     writeChunk(&chunk, OP_RETURN, 124);
 
     disassembleChunk(&chunk, "test chunk");
 
+    interpret(&chunk);
+
     freeChunk(&chunk);
+    freeVM();
     return 0;
 }
---
+++ clox/vm.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+#include "chunk.h"
+#include "common.h"
+#include "debug.h"
+#include "vm.h"
+
+VM vm;
+
+static void resetStack() {
+    vm.stackTop = vm.stack;
+}
+
+void initVM() {
+    resetStack();
+}
+
+void freeVM() {}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
+static InterpretResult run() {
+    #define READ_BYTE() (*vm.ip++)
+    #define READ_CONST() (vm.chunk->constants.values[READ_BYTE()])
+
+    for (;;) {
+    #ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+    #endif
+
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+            case OP_CONST: {
+                Value constant = READ_CONST();
+                push(constant);
+                break;
+            }
+            case OP_ADD: {
+                Value a = pop();
+                Value b = pop();
+                push(a + b);
+                break;
+            }
+            case OP_SUBTRACT: {
+                Value a = pop();
+                Value b = pop();
+                push(b - a);
+                break;
+            }
+            case OP_MULTIPLY: {
+                Value a = pop();
+                Value b = pop();
+                push(b * a);
+                break;
+            }
+            case OP_DIVIDE: {
+                Value a = pop();
+                Value b = pop();
+                push(b / a);
+                break;
+            }
+            case OP_NEGATE: {
+                Value a = pop();
+                push(-a);
+                break;
+            }
+            case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
+                return INTERPRET_OK;
+            }
+        }
+    }
+
+    #undef READ_BYTE
+    #undef READ_CONST
+}
+
+InterpretResult interpret(Chunk *chunk) {
+    vm.chunk = chunk;
+    vm.ip = chunk->code;
+    return run();
+}       
